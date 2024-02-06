671a14ec2a5d6ffd4af124c013f94398bea6111f
jjuanramos <jjuanramos91@gmail.com>
2024-02-06 12:42:54 +0100

    we no longer print errors, but pass them to stderr. Also, now we make sure no duplicated params are passed

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,12 +6,14 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 10:35:27 by juramos           #+#    #+#             */
-/*   Updated: 2024/02/06 12:08:20 by juramos          ###   ########.fr       */
+/*   Updated: 2024/02/06 12:36:51 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+
+# define LEN_MAX_INT 10
 
 # include "include/ft_printf.h"
 # include "include/libft.h"
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 11:12:00 by juramos           #+#    #+#             */
-/*   Updated: 2024/02/06 12:18:48 by juramos          ###   ########.fr       */
+/*   Updated: 2024/02/06 12:42:03 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,19 @@ int	is_str_digit(char *str)
 	return (1);
 }
 
+int	is_arg_unique(char *arr[], int pos)
+{
+	char	*ref;
+
+	ref = arr[pos];
+	while (arr[++pos])
+	{
+		if (ft_strncmp(ref, arr[pos], LEN_MAX_INT) == 0)
+			return (0);
+	}
+	return (1);
+}
+
 int	*convert_to_arr(int argc, char	*argv[])
 {
 	int	*arr;
@@ -38,17 +51,18 @@ int	*convert_to_arr(int argc, char	*argv[])
 	arr = ft_calloc(argc, sizeof(int));
 	if (!arr)
 	{
-		ft_printf("Error\n");
+		ft_putstr_fd("Error\n", 2);
 		exit(1);
 	}
 	argv_pos = 0;
 	while (argv[++argv_pos])
 	{
-		if (is_str_digit(argv[argv_pos]))
+		// todo: pending to check whether arg is greater than max int 
+		if (is_str_digit(argv[argv_pos]) && is_arg_unique(argv, argv_pos))
 			arr[argv_pos - 1] = ft_atoi(argv[argv_pos]);
 		else
 		{
-			ft_printf("Error\n");
+			ft_putstr_fd("Error\n", 2);
 			free(arr);
 			exit(1);
 		}
@@ -61,6 +75,8 @@ void	print_arr(int *arr)
 	int	pos;
 
 	pos = 0;
+	// zero is currently not being printed - check whether it is due to
+	// printing reasons or a failure in the atoi.
 	while (arr[pos])
 	{
 		ft_printf("%d ", arr[pos]);
@@ -75,18 +91,9 @@ int	main(int argc, char *argv[])
 
 	if (argc < 2)
 	{
-		ft_printf("Error\n");
+		ft_putstr_fd("Error\n", 2);
 		exit(1);
 	}
-	/* TODO:
-		We must assign a dynamically sized array of ints, whose size
-		depends on the number of arguments passed to the executable.
-		- atoi for each string.
-			- error and exit if isdigit fails for any of the characters of each string passed.
-		- we can use argc to attribute a calloc size of that value to our array. (actual size is
-		argc - 1, but we want to leave an space for a \0)
-
-	*/
 	else
 	{
 		arr = convert_to_arr(argc, argv);
@@ -94,7 +101,7 @@ int	main(int argc, char *argv[])
 		if (!arr)
 		// there is a function to be created, message_and_exit or smth like that
 		{
-			ft_printf("Error\n");
+			ft_putstr_fd("Error\n", 2);
 			exit(1);
 		}
 	}
