dbdb183692fc480d3160bfe5d98bdc385c8b5b5f
jjuanramos <jjuanramos91@gmail.com>
2024-02-15 11:43:51 +0100

    fixed a bug that parsed characters

--- src/args_parse.c
+++ src/args_parse.c
@@ -6,7 +6,7 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 15:24:13 by juramos           #+#    #+#             */
-/*   Updated: 2024/02/13 18:12:27 by juramos          ###   ########.fr       */
+/*   Updated: 2024/02/15 11:43:33 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ static int	is_str_digit(char *str)
 	while (str[++pos])
 	{
 		if (!ft_isdigit(str[pos])
-			&& (str[pos] == '-' && pos != 0))
+			&& !(str[pos] == '-' && pos == 0))
 			return (0);
 	}
 	return (1);
