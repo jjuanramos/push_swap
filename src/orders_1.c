025f2894db417216330236e1432d362f1c36fb02
jjuanramos <jjuanramos91@gmail.com>
2024-02-15 10:58:22 +0100

    added yet another condition

--- src/orders_1.c
+++ src/orders_1.c
@@ -6,7 +6,7 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 13:51:48 by juramos           #+#    #+#             */
-/*   Updated: 2024/02/14 13:33:41 by juramos          ###   ########.fr       */
+/*   Updated: 2024/02/15 10:56:13 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,7 +81,8 @@ t_stack	*order_a(t_stack *stack_a, t_stack *stack_b)
 
 	head_a = get_head(stack_a);
 	stack_size = get_stack_size(head_a);
-	if (stack_size <= 3)
+	if (stack_size <= 3 && (stack_b && get_min_to_right(head_a)
+			> get_max_to_right(get_head(stack_b))))
 		check_three(stack_a);
 	else if (head_a->next && (head_a->next)->value == get_min_to_right(head_a)
 		&& stack_b
