10c05606bee3400dbba43de696c92dcdf4392e08
jjuanramos <jjuanramos91@gmail.com>
2024-02-23 12:54:06 +0100

    remove stdlib

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 10:35:27 by juramos           #+#    #+#             */
-/*   Updated: 2024/02/22 11:12:34 by juramos          ###   ########.fr       */
+/*   Updated: 2024/02/23 12:52:34 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@
 
 # include "include/ft_printf.h"
 # include "include/libft.h"
-# include <stdlib.h>
 
 typedef struct s_stack {
 	struct s_stack	*prev;
