b33e75938b88dddd1f32b1ceeba08ed98fc8c44f
juramos <juramos@student.42madrid.com>
2024-02-26 10:19:30 +0100

    increased size

--- src/quicksort.c
+++ src/quicksort.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   quicksort.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
+/*   By: juramos <juramos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 11:25:37 by juramos           #+#    #+#             */
-/*   Updated: 2024/02/22 11:27:50 by juramos          ###   ########.fr       */
+/*   Updated: 2024/02/26 10:17:48 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	quicksort(t_stack *stack_a)
 	a_size = get_stack_size(get_head(stack_a));
 	if (a_size > 250)
 		chunks = 12;
-	else if (a_size > 49)
+	else if (a_size > 50)
 		chunks = 6;
 	else
 		chunks = 1;
