445c7f9c70104c2862e1c34a2dcddce74bd2e0d4
juramos <juramos@student.42madrid.com>
2024-02-26 11:19:00 +0100

    fixed issue when main receives string as input

--- src/main.c
+++ src/main.c
@@ -3,22 +3,52 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
+/*   By: juramos <juramos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 11:12:00 by juramos           #+#    #+#             */
-/*   Updated: 2024/02/22 11:39:04 by juramos          ###   ########.fr       */
+/*   Updated: 2024/02/26 11:18:44 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+static int	str_arr_len(char **str_arr)
+{
+	int	len;
+
+	len = 0;
+	while (str_arr[len])
+		len++;
+	return (len);
+}
+
+static	void	free_str_arr(char **str_arr, int len)
+{
+	while (--len)
+		free(str_arr[len]);
+	free(str_arr[len]);
+	free(str_arr);
+}
+
 int	main(int argc, char *argv[])
 {
 	t_stack	*stack_a;
+	char	**helper;
+	int		len;
 
 	if (argc >= 2)
 	{
-		stack_a = parse_to_stck(argc, argv);
+		helper = NULL;
+		len = 0;
+		if (argc == 2 && ft_strnstr(argv[1], " ", ft_strlen(argv[1])))
+		{
+			helper = ft_split(argv[1], ' ');
+			len = str_arr_len(helper);
+			stack_a = parse_to_stck(len, helper);
+			free_str_arr(helper, len);
+		}
+		else
+			stack_a = parse_to_stck(argc, argv);
 		if (!stack_a)
 			message_and_exit("Error\n", 2, 1);
 		quicksort(stack_a);
