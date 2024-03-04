967219341160fbfeb5688c6f19f20af6cdfd6d47
jjuanramos <jjuanramos91@gmail.com>
2024-03-04 10:17:30 +0100

    added printers

--- src/stack_2.c
+++ src/stack_2.c
@@ -6,7 +6,7 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 13:51:48 by juramos           #+#    #+#             */
-/*   Updated: 2024/03/01 13:50:41 by juramos          ###   ########.fr       */
+/*   Updated: 2024/03/04 10:17:14 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,21 @@ void	check_three(t_stack *stack_a)
 
 	a = get_head(stack_a);
 	if (a->value > (a->next)->value && a->value < (get_tail(a))->value)
+	{
 		sa(stack_a);
+		ft_printf("sa\n");
+	}
 	else if (a->value < (a->next)->value
 		&& (a->next)->value > (get_tail(a))->value)
+	{
 		rra(stack_a);
+		ft_printf("rra\n");
+	}
 	else
+	{
 		ra(stack_a);
+		ft_printf("ra\n");
+	}
 }
 
 t_stack	*get_min_to_right(t_stack *stck)
